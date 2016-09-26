13a8e74402b08c30f68069fd3ac308eab3c65092
RyosukeCla <ryosukeclarinet@gmail.com>
2016-09-26 11:40:19 +0900

    fix

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@
 
 TAG=1.3
 
-docker build -t ryosukecla/jupyter-tensorflow:latest .
-docker tag ryosukecla/jupyter-tensorflow:latest ryosukecla/jupyter-tensorflow:${TAG}
+docker build -t ryosukecla/jupyter-tensorflow:latest ./
 
